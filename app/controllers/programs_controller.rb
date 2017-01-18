c2cf9d519096515f72641933d36959112c061674
Raghu Betina <rbetina@gmail.com>
2017-01-18 14:07:28 -0600

    Update permanent programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,7 +2,11 @@ class ProgramsController < ApplicationController
   def permanent_programs
     # Your code goes here.
 
-    @your_output = "Replace this string with your output"
+    my_birthday = Time.parse("July 1st, 2000")
+    today = Time.now
+    seconds_since_i_was_born = today - my_birthday
+
+    @your_output = seconds_since_i_was_born
 
     render("programs/permanent_programs.html.erb")
   end
