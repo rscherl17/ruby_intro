cfe01cc4a93bcf9a9093ca74f2ec6325e48fc642
Raghu Betina <rbetina@gmail.com>
2017-01-18 12:38:10 -0600

    Start fundamental classes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,10 @@ class PagesController < ApplicationController
 
     render("pages/primary_syntax.html.erb")
   end
+
+  def fundamental_classes
+    @chapter_content = File.open(Rails.root.join("lib", "markdown", "fundamental_classes.md")).read
+
+    render("pages/fundamental_classes.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "welcome" })
+
   get("/primary_syntax", { :controller => "pages", :action => "primary_syntax" })
+
+  get("/fundamental_classes", { :controller => "pages", :action => "fundamental_classes" })
 end
