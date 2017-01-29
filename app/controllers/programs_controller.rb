e30dc07b9312a27b6bd354afc8d4e361f4690f20
rscherl17 <robertscherl@wireless-s0-no-150-0-163.uchicago.edu>
2017-01-29 17:37:05 -0600

    r

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,16 +1,20 @@
 class ProgramsController < ApplicationController
   def permanent_programs
-    # Your code goes here.
+    my_birthday = Time.parse("July 1st, 2000")
+      today = Time.now
+      seconds_since_i_was_born = today - my_birthday
 
-    @your_output = "Replace this string with your output"
+    @your_output = "seconds_since_i_was_born."
 
     render("programs/permanent_programs.html.erb")
   end
 
   def conditionals
-    # Your code goes here
+    if 1<2
+      message = "duh"
+    end
 
-    @your_output = "Replace this string with your output"
+    @your_output = message
 
     render("programs/conditionals.html.erb")
   end
